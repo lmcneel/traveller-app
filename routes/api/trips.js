6cc50373b3787d7184600014123e60f1305b415e
Latisha McNeel <latisha.mcneel@gmail.com>
2021-03-09 21:00:54 -0600

    Trip Routes are complete

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -1,11 +1,39 @@
 const router = require('express').Router();
+const to = require('await-to-js').default;
+const sequelize = require('../../config/connection');
+const { Traveller, Location, Trip } = require('../../models');
 
-router.post('/', (req, res) => {
-        res.send('POST API TRIPS');
-    });
+router.post('/', async (req, res) => {
+    const {
+        trip_budget,
+        traveller_amount,
+        traveller_id,
+        location_id
+    } = req.body;
 
-router.delete('/:id', (req, res) => {
-        res.send('DELETE API A TRIP');
-    });
+    const [err, trip] = await to(Trip.create({
+        trip_budget,
+        traveller_amount,
+        traveller_id,
+        location_id
+    }));
+
+    if (err) return res.status(500).json(err);
+    res.json(trip);
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const [err, trip] = await to(Trip.destroy({
+        where: {
+            id
+        }
+    }));
+
+    if (err) return res.status(500).json(err);
+
+    res.json(trip);
+});
 
 module.exports = router;
