943327cd7e7dc91598e7214224917cce44f98704
Latisha McNeel <latisha.mcneel@gmail.com>
2021-03-09 18:57:53 -0600

    Base express server with default route

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    const instructions = `
+        <h1>Hello!</h1>
+        <p>This is an traveller's api</p>
+        <p>The following routes are available:</p>
+        <ul>
+            <li>GET route /api/travellers</li>
+            <li>POST route /api/travellers</li>
+            <li>GET route /api/travellers/:id</li>
+            <li>DELETE route /api/travellers/:id</li>
+            <li>GET route /api/locations</li>
+            <li>POST route /api/locations</li>
+            <li>GET route /api/locations/:id</li>
+            <li>DELETE route /api/locations/:id</li>
+            <li>POST route /api/trips</li>
+            <li>DELETE route /api/trips/:id</li>
+        </ul>
+    `;
+
+    res.send(instructions);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(routes);
+
+
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`);
+});
